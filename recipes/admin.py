750ec64d8d2734b9efa1a6551c74193d0f7c5964
Greg Robertson <grobertson743@gmail.com>
2022-04-20 15:43:21 -0700

    Added TheFoodItem, Measure, and Ingredient Models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from recipes.models import Recipe
+from recipes.models import Recipe, Measure, FoodItem, Ingredient
 
 # Register your models here.
 
@@ -8,4 +8,19 @@ class RecipeAdmin(admin.ModelAdmin):
     pass
 
 
+class MeasureAdmin(admin.ModelAdmin):
+    pass
+
+
+class FoodItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Measure, MeasureAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -5,12 +5,36 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=125, default="default name")
-    author = models.CharField(max_length=100, default="default author")
-    description = models.TextField(default="default description")
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
     image = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField()
+    recipe = models.ForeignKey(
+        "recipe", related_name="ingredients", on_delete=models.CASCADE
+    )
+    measure = models.ForeignKey("measure", on_delete=models.PROTECT)
+    food = models.ForeignKey("fooditem", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.amount
