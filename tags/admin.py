cd6c8ddefe90004c73e7e9b88b60c9ddacb57cff
Greg Robertson <grobertson743@gmail.com>
2022-04-23 12:30:55 -0700

    Requirements.txt change

--- tags/admin.py
+++ tags/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 
+# from recipes.models import Tag
 
 # Register your models here.
+
+
+# class TagAdmin(admin.ModelAdmin):
+#     pass
+
+
+# admin.site.register(Tag, TagAdmin)
