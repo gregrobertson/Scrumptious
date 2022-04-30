2df3eab633244951d09156602d02b628d66be00d
Greg Robertson <grobertson743@gmail.com>
2022-04-29 17:02:48 -0700

    Resizer Function v2

--- recipes/test_filters.py
+++ recipes/test_filters.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from recipes.models import Ingredient, Recipe
 
-# from recipes.templatetags.resizer import resize_to
+from recipes.templatetags.resizer import resize_to
 
 
 class ResizeToTests(TestCase):
