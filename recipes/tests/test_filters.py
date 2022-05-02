79d28a60a44bbb53bb18c9c70e855899c05aee51
Greg Robertson <grobertson743@gmail.com>
2022-05-02 16:42:07 -0700

    Unit Tests Finished

--- recipes/tests/test_filters.py
+++ recipes/tests/test_filters.py
@@ -5,4 +5,42 @@ from recipes.templatetags.resizer import resize_to
 
 
 class ResizeToTests(TestCase):
-    pass
+    def test_no_resize(self):
+        with self.assertRaises(AttributeError):
+            resize_to(None, 3)
+
+    def test_recipe_has_no_serving(self):
+        # Arrange
+        recipe = Recipe(servings=None)
+        ingredient = Ingredient(recipe=recipe, amount=5)
+        # Act
+        result = resize_to(ingredient, None)
+        # Assert
+        self.assertEqual(5, result)
+
+    def test_resize_to_is_none(self):
+        # Arrange
+        recipe = Recipe(servings=2)
+        ingredient = Ingredient(recipe=recipe, amount=5)
+        # Act
+        result = resize_to(ingredient, None)
+        # Assert
+        self.assertEqual(5, result)
+
+    def test_values_for_servings_amount_and_target(self):
+        # Arrange
+        recipe = Recipe(servings=2)
+        ingredient = Ingredient(recipe=recipe, amount=5)
+        # Act
+        result = resize_to(ingredient, 10)
+        # Assert
+        self.assertEqual(25, result)
+
+    def test_target_is_letters(self):
+        # Arrange
+        recipe = Recipe(servings=2)
+        ingredient = Ingredient(recipe=recipe, amount=5)
+        # Act
+        result = resize_to(ingredient, "abc")
+        # Assert
+        self.assertEqual(5, result)
